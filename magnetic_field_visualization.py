c79c981d8957787990c7e3261740f80eb0659f23
gk569058248-svg <gk569058248@gmail.com>
2026-03-23 13:31:30 +0800

    Create magnetic_field_visualization.py for magnetic field visualization

---
+++ magnetic_field_visualization.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+# 设置环境变量，避免PIL错误
+# os.environ['STREAMLIT_WATCHER_TYPE'] = 'none'
+
+# 设置中文字体
+plt.rcParams['font.sans-serif'] = ['SimHei', 'Microsoft YaHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+st.title("生成磁场强度随时间变化图")
+upload_file = st.file_uploader("上传数据文件", type=["txt"])
+if upload_file:
+    df = pd.read_csv(upload_file, sep=r'\s+', skiprows=1, header=None, encoding='gbk')
+    time = df[0]
+    mag = df[1]
+    fig, ax = plt.subplots()
+    ax.plot(time, mag)
+
+    ax.set_xlabel("时间 （s）")
+    ax.set_ylabel("磁场强度（pT）")
+    ax.set_title("磁场强度随时间变化图")
+    st.pyplot(fig)
